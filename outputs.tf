d653c834f2a0d63b536adfb6648e6659b8d02b21
dkooll <dennis.kool@cloudnation.nl>
2026-04-03 08:41:25 +0200

    feat: add missing outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,23 @@ output "solutions" {
   description = "contains log analytics solutions"
   value       = azurerm_log_analytics_solution.solutions
 }
+
+output "tables" {
+  description = "contains log analytics workspace tables"
+  value       = azurerm_log_analytics_workspace_table.tables
+}
+
+output "export_rules" {
+  description = "contains log analytics data export rules"
+  value       = azurerm_log_analytics_data_export_rule.rule
+}
+
+output "linked_service" {
+  description = "contains log analytics linked service"
+  value       = azurerm_log_analytics_linked_service.link
+}
+
+output "linked_storage" {
+  description = "contains log analytics linked storage accounts"
+  value       = azurerm_log_analytics_linked_storage_account.link
+}
